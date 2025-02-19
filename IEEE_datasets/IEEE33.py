4cb75ba26370f50766a1dc5e19a9bf0e7873f3bb
kyriakos <kyriakosandresakis@gmail.com>
2025-02-19 11:28:19 +0200

    add folder

---
+++ IEEE_datasets/IEEE33.py
@@ -0,0 +1,57 @@
+config_dict = {
+    # Open branch at index i-1 for branch i (mapping for pandapower)
+    "IEEE33": {
+        "T1": {
+            "open_branches": [32, 33, 34]
+        },
+        "T2": {
+            "open_branches": [6, 33, 34]
+        },
+        "T3": {
+            "open_branches": [10, 32, 34]
+        },
+        "T4": {
+            "open_branches": [6, 10, 34]
+        },
+        "T5": {
+            "open_branches": [27, 32, 33]
+        },
+        "T6": {
+            "open_branches": [6, 27, 33]
+        },
+        "T7": {
+            "open_branches": [10, 27, 32]
+        },
+        "T8": {
+            "open_branches": [6, 10, 27]
+        },
+        "T9": {
+            "open_branches": [33, 34]
+        },
+        "T10": {
+            "open_branches": [32, 34]
+        },
+        "T11": {
+            "open_branches": [34]
+        },
+        "T12": {
+            "open_branches": [32, 33]
+        },
+        "T13": {
+            "open_branches": [33]
+        },
+        "T14": {
+            "open_branches": [32]
+        },
+        "T15": {
+            "open_branches": []
+        },
+    },
+    "MESOGEIA": {
+        "T1": {
+            "open_branches": [131]
+        },
+        "T2": {
+            "open_branches": []
+        },},
+}
